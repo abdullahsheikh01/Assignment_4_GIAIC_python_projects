dfb49a44e3335aeecc259eb52554d2b9604e7405
abdullahsheikh01 <162831270+abdullahsheikh01@users.noreply.github.com>
2025-03-19 10:49:02 +0500

    First Project Complete✅

---
+++ 01_add_two_numbers/main.py
@@ -0,0 +1,14 @@
+# This is a program to sum two numbers
+def main():
+    try:
+        num1 : float = float(input("Enter First Number: "))
+        num2 : float = float(input("Enter Second Number: "))
+        sum : float = num1+num2
+        print(f"The sum of {num1} and {num2} is {sum}")
+    # Except Block for Value Error 
+    except:
+        print("Give Values in Numbers")
+        main()
+# This ensures that the following code runs only when the script is executed directly
+if __name__ == '__main__':
+    main()
