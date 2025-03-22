203e9604a47cfd945412372930bc2fa573a15ba7
abdullahsheikh01 <162831270+abdullahsheikh01@users.noreply.github.com>
2025-03-22 16:28:15 +0500

    Project 08_tiny_mad_lib done✅

---
+++ Assignment1/01_expressions/08_tiny_mad_lib/README.md
@@ -0,0 +1,4 @@
+# 08_tiny_mad_lib
+This Python program prompts the user for an **adjective, noun, and verb**, then generates a fun sentence using the provided words.
+## Program Code Link(Google Colab):
+https://colab.research.google.com/drive/1FTPUWHp3VHAM_fijvwo--Nx_AAcR_JHm?usp=sharing
---
+++ Assignment1/01_expressions/08_tiny_mad_lib/main.py
@@ -0,0 +1,23 @@
+# Problem: Write a program which prompts the user for an adjective, then a noun,
+# then a verb, and then prints a fun sentence with those words!
+import sys
+def main():
+  BOLD = "\033[1m"
+  RESET = "\033[0m" 
+  BOLD_ITALIC = "\033[1;3m"
+  try:
+      SENTENCE_START: str = "Panaversity is fun. I learned to program and used Python to make my "
+      sys.stdout.write("Enter an adjective "+BOLD_ITALIC)
+      adjective : str = input()
+      sys.stdout.write(RESET)
+      sys.stdout.write("Enter a noun "+BOLD_ITALIC)
+      noun : str = input()
+      sys.stdout.write(RESET)
+      sys.stdout.write("Enter a verb "+BOLD_ITALIC)
+      verb : str = input()
+      sys.stdout.write(RESET)
+      print(f"{SENTENCE_START}{BOLD_ITALIC}{adjective} {noun} {verb}{RESET}!")
+  except:
+      print("Unknown Error Occured!")
+if __name__ == '__main__':
+    main()
