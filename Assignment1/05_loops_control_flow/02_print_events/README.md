7f520fd98d7e12fee7ae08fcdac2d0b95a1bdeae
abdullahsheikh01 <162831270+abdullahsheikh01@users.noreply.github.com>
2025-03-31 16:15:33 +0500

    Project 02_print_events done✅

---
+++ Assignment1/05_loops_control_flow/02_print_events/README.md
@@ -0,0 +1,5 @@
+# **02_print_events**
+This Python program prints the first 20 even numbers starting from 0.
+
+## Program Code Link(Google Colab):
+https://colab.research.google.com/drive/1ATmwLTiX5kqzzaD2loGN7J4nUi2qc0uQ?usp=sharing
---
+++ Assignment1/05_loops_control_flow/02_print_events/main.py
@@ -0,0 +1,14 @@
+"""
+Problem: Write a program that prints the first 20 even numbers. 
+There are several correct approaches, but they all use a loop of some sort. 
+Do no write twenty print statements
+"""
+def main():
+    try:
+      print("The First 20 even numbers are: ")
+      for _ in range(0,20):
+        print(_*2)
+    except:
+       print("Unknown Error Occured!")
+if __name__ == '__main__':
+    main()
